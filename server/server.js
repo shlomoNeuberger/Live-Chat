9cad8179f68290d586adf1c750e78f95d24f0c56
Shlomo Neuberger <shlomo.dev@neuberger.co.il>
2021-04-12 14:52:59 +0300

    Setting the user model and starting server

--- server/server.js
+++ server/server.js
@@ -1,4 +1,31 @@
 const express = require('express');
 const mongoose = require('mongoose')
-const dotenv = require('dotenv')
+require('dotenv').config()
 const app = express()
+
+const PORT = process.env.PORT
+const db = process.env.MON_DB || "mongodb://localhost:27017/test"
+
+mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
+console.log(`We are using ${db} Database`);
+
+
+const UserSchema = new mongoose.Schema(
+    {
+        username: String,
+        Email: String,
+        Massages: [],
+    }
+)
+
+const User = mongoose.model('User', UserSchema)
+
+app.listen(PORT, () => {
+    console.log(`start server at port ${PORT}`);
+})
+
+app.get("/", (req, res) => {
+    res.send("<h1>Hello World</h1>")
+})
