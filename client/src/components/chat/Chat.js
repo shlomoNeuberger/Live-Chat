cea773dfc98f2479694c1d26096490c9b7462f6f
Shlomo Neuberger <shlomo.dev@neuberger.co.il>
2021-04-13 22:17:31 +0300

    Changed Chat from function to class component for better contorl on lifecycle, update the css and the design to work with tables and not only with float, and also splitted the Messages to another component

--- client/src/components/chat/Chat.js
+++ client/src/components/chat/Chat.js
@@ -1,7 +1,9 @@
-import React from 'react'
-import { Redirect, useParams } from 'react-router-dom'
+import React, { } from 'react'
+import { Redirect } from 'react-router-dom'
 import './Chat.css'
 
+import socketIOClient from 'socket.io-client'
+import Message from './Message'
 // function makeid(length) {
 //     var result = [];
 //     var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
@@ -14,53 +16,124 @@ import './Chat.css'
 // }
 
 
-function Users({ user, setUser }) {
-    const { roomId, roomName } = useParams()
-    console.log(roomId);
-    console.log(roomName);
-    if (user.active)
-        return (
-            <div className="container-fluid" >
-                <div className="outer-box  mx-auto ">
-                    <div className="chat-box-feild overflow-auto">
-                        {
-                            //In: classname float-start msg in
-                            //Out: classname float-end msg out
-                            //Join: classname msg join mx-auto
-                        }
-                        <div className="msg join mx-auto"><span>in:Hello How Are you</span> </div><br />
-                        <div className="float-start msg in"><span>in:Hello How Are you</span> </div><br />
-                        <div className="float-end msg out"><span>out:I'm ok 10x</span> </div><br />
-                        <div className="float-start msg in"><span>in:Hello How Are you</span> </div><br />
-                        <div className="float-end msg out"><span>out:I'm ok 10x</span> </div><br />
-                        <div className="float-start msg in"><span>in:Hello How Are you</span> </div><br />
-                        <div className="float-end msg out"><span>out:I'm ok 10x</span> </div><br />
-                        <div className="float-start msg in"><span>in:Hello How Are you</span> </div><br />
-                        <div className="float-end msg out"><span>out:I'm ok 10x</span> </div><br />
-                        <div className="float-start msg in"><span>in:Hello How Are you</span> </div><br />
-                        <div className="float-end msg out"><span>out:I'm ok 10x</span> </div><br />
+const ENDPOINT = "ws://127.0.0.1:3001/"
+
+
+class Chat extends React.Component {
+    socket
+    roomSlug
+    async endlessUpdate() {
+        this.setState({ msg: this.state.msg })
+        setTimeout(() => {
+            this.endlessUpdate()
+        }, Math.random() * 5000);
+    }
+    constructor(props) {
+        super(props);
+        if (this.props.location)
+            this.roomSlug = this.props.location.pathname.replace('/room/', '')
+        this.state = {
+            msg: "",
+            messages: [],
+        }
+        this.endlessUpdate()
+
+    }
+    componentDidCatch() {
+        console.log("Did Catch")
+    }
+    componentDidMount() {
+        const joinRoom = {
+            room: this.roomSlug,
+            type: 'join',
+            user: this.props.user,
+            timestemp: Date.now(),
+        }
+        console.log("Did Mount")
+        this.socket = socketIOClient(ENDPOINT);
+        this.socket.emit("conect room", joinRoom)
+
+        this.socket.on('msg', (args) => {
+            let tArray = [...this.state.messages]
+            tArray.push(args)
+            this.setState({ messages: tArray })
+
+        });
+    }
+    componentDidUpdate() {
+    }
+    componentWillUnmount() {
+        this.socket.disconnect()
+    }
+    /** 
+    *
+    * @param { Event } e sender event
+    */
+    submitHandle(e) {
+        e.preventDefault()
+        if (this.state.msg !== '') {
+            this.setState({ msg: '' })
+            const message = {
+                msg: this.state.msg,
+                user: this.props.user,
+                room: this.roomSlug,
+                timestemp: Date.now(),
+                type: 'msg'
+            }
+            this.socket.emit('msg', message)
+        }
+    }
+
+
+
+
+    render() {
+        if (this.props.user.active && this.props.location) {
+            return (
+                <div className="container-fluid" >
+                    <div className="outer-box  mx-auto ">
+                        <div className="chat-box-feild p-2 overflow-auto container-fluid">
+                            {
+                                this.state.messages.map(e =>
+                                    <Message key={e.id} e={e} user={this.props.user} />
+
+                                )
+                            }
+                        </div>
+                        <form className="form-controls" onSubmit={e => this.submitHandle(e)}>
+                            <div className="input-group chat-box-message">
+                                <span className="input-group-text" id="basic-addon1">Message</span>
+                                <input
+                                    name="msg"
+                                    className="form-control"
+                                    placeholder="Insert your message..."
+                                    aria-label="msg"
+                                    value={this.state.msg}
+                                    onChange={e => { this.setState({ msg: e.target.value }) }}
+                                    aria-describedby="basic-addon1" />
+                                <button
+                                    type="submit"
+                                    className="btn btn-outline-success"
+
+                                >
+                                    Send
+                                </button>
+                            </div>
+                        </form>
                     </div>
-                    <div className="input-group mb-3 chat-box-message">
-                        <span className="input-group-text" id="basic-addon1">Message</span>
-                        <input
-                            type="msg"
-                            name="msg"
-                            className="form-control"
-                            placeholder="Insert your message..."
-                            aria-label="msg"
-                            aria-describedby="basic-addon1" />
-                        <button type="submit" className="btn btn-outline-success" >Send</button>
-                    </div>
-                </div>
-            </div >
-        );
-    else
-        return (
-            <Redirect to={{
-                pathname: "/",
-                state: { src: 'Chat' },
-            }} />
-        );
+                </div >
+            );
+        }
+        else
+            return (
+                <Redirect to={{
+                    pathname: "/",
+                    state: { src: 'Chat' },
+                }} />
+            );
+    }
 }
 
-export default Users
+
+
+export default Chat
