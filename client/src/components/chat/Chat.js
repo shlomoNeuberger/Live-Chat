135d65c6c78a34e5c675fb9754f550ef675a51f7
Shlomo Neuberger <shlomo.dev@neuberger.co.il>
2021-04-17 19:27:37 +0300

    imporved logic for communication btween server socket and Chat compnent, for prventing double login, added side bar with connected users

--- client/src/components/chat/Chat.js
+++ client/src/components/chat/Chat.js
@@ -4,25 +4,39 @@ import './Chat.css'
 import socketIOClient from 'socket.io-client'
 import Message from './Message'
 import env from 'react-dotenv'
-
+import AlredyAlert from './AlredyAlert'
+import Users from '../users/Users'
 
 
 const ENDPOINT = env.BASE_IO_SOCKET
+
+function convertToslugFromUrl(url) {
+
+    let splited = url.replace('/room/', '').split('/');
+    const roomName = splited[0];
+    const roomSlug = splited[1]
+
+    return roomName.split(' ').join('-') + '_' + roomSlug
+}
 
 class Chat extends React.Component {
     messagesEndRef = React.createRef()
     socket
     roomSlug
+    redirect = null
+
     constructor(props) {
         super(props);
         if (this.props.location)
-            this.roomSlug = this.props.location.pathname.replace('/room/', '')
+            this.roomSlug = convertToslugFromUrl(this.props.location.pathname)
         this.state = {
             msg: "",
             messages: [],
+            users: [],
         }
 
     }
+
     componentDidMount() {
         const joinRoom = {
             room: this.roomSlug,
@@ -39,23 +53,33 @@ class Chat extends React.Component {
                 tArray.push(args)
                 this.setState({ messages: tArray })
                 this.scrollIntoEndChat()
-            });
+            })
+            .on('already in', () => {
+                this.redirect = <AlredyAlert />
+            })
+            .on('update user', (u) => {
+                console.log(Date.now(), u);
+                this.setState({ users: u })
+            })
     }
 
     scrollIntoEndChat() {
         this.messagesEndRef.current.scrollIntoView({ behavior: 'smooth' })
     }
 
+
     componentDidUpdate() {
 
     }
+
     componentWillUnmount() {
         this.socket.disconnect()
     }
-    /** 
-    *
-    * @param { Event } e sender event
-    */
+
+    /**
+     *
+     * @param { Event } e sender event
+     */
     submitHandle(e) {
         e.preventDefault()
         if (this.state.msg !== '') {
@@ -65,6 +89,7 @@ class Chat extends React.Component {
                 msg: this.state.msg,
                 user: this.props.user,
                 room: this.roomSlug,
+                roomSlug: this.roomSlug,
                 timestemp: Date.now(),
                 type: 'msg'
             }
@@ -73,14 +98,12 @@ class Chat extends React.Component {
     }
 
 
-
-
-
     render() {
         if (this.props.user.active && this.props.location) {
             return (
-                <div className="container-fluid " >
-                    <div className="outer-box mx-auto  ">
+                <div className="container-fluid row ">
+                    {this.redirect ? this.redirect : null}
+                    <div className="outer-box col-10">
                         <div className="chat-box-feild px-4 overflow-auto">
                             {
                                 this.state.messages.map(e =>
@@ -89,31 +112,37 @@ class Chat extends React.Component {
                             }
                             <div ref={this.messagesEndRef}></div>
                         </div>
-                        <form className="chat-box-message" onSubmit={e => this.submitHandle(e)}>
-                            <div className="input-group ">
-                                <span className="input-group-text" id="basic-addon1">Message</span>
-                                <input
-                                    name="msg"
-                                    type="text"
-                                    className="form-control"
-                                    placeholder="Insert your message..."
-                                    aria-label="msg"
-                                    value={this.state.msg}
-                                    onChange={e => { this.setState({ msg: e.target.value }) }}
-                                    aria-describedby="basic-addon1" />
-                                <button
-                                    type="submit"
-                                    className="btn btn-outline-success"
-                                >
-                                    Send
-                                </button>
-                            </div>
-                        </form>
+                        {!this.redirect ?
+                            <form className="chat-box-message" onSubmit={e => this.submitHandle(e)}>
+                                <div className="input-group ">
+                                    <span className="input-group-text" id="basic-addon1">Message</span>
+                                    <input
+                                        name="msg"
+                                        type="text"
+                                        className="form-control"
+                                        placeholder="Insert your message..."
+                                        aria-label="msg"
+                                        value={this.state.msg}
+                                        onChange={e => {
+                                            this.setState({ msg: e.target.value })
+                                        }}
+                                        aria-describedby="basic-addon1" />
+                                    <button
+                                        type="submit"
+                                        className="btn btn-outline-success"
+                                    >
+                                        Send
+                                    </button>
+                                </div>
+                            </form> : null
+                        }
                     </div>
-                </div >
+                    <div className="outer-box col-2">
+                        <Users users={this.state.users} />
+                    </div>
+                </div>
             );
-        }
-        else
+        } else
             return (
                 <Redirect to={{
                     pathname: "/",
@@ -122,7 +151,6 @@ class Chat extends React.Component {
             );
     }
 }
-
 
 
 export default Chat
--- server/server.js
+++ server/server.js
@@ -4,23 +4,21 @@ const http = require('http')
 const socketIO = require('socket.io');
 const api = require('./api');
 const mongoose = require('mongoose');
-const cors = require('cors')
+const cors = require('cors');
 
 require('dotenv').config()
 
 const app = express()
 const server = http.createServer(app);
 const io = socketIO(server, {
-    cors: {
-    }
+    cors: {}
 })
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(api)
+app.use(api.route)
 app.use(cors())
 
 //app.use(express.static('build'))
-
 
 
 const PORT = process.env.PORT
@@ -45,67 +43,108 @@ function update() {
 io.on('connection', (socket) => {
     update()
     console.log("Connected " + socket.id);
-    socket.on('msg', (msg) => {
+    socket
+        .on('msg', (msg) => {
+            io.to(msg.room).emit('msg', msg)
+        })
+        .on('conect room', (args) => {
+            socket.user = args.user
+            //#region cheack if user us alredy in room if not add him
+            Room.findOne({ slug: args.room }, (err, room) => {
+                if (err) {
+                    console.log(err)
+                } else {
+                    User.findById(args.user.id, (err, user) => {
+                        if (err) {
+                            console.log(err);
+                        } else {
+                            const users = room.get('users') ?? []
+                            const inUsers = users.filter((value, index, array) => String(user._id) === String(value._id))
+                            const nUsers = users.filter((value, index, array) => String(user._id) !== String(value._id))
+                            console.log(`inUsers`, inUsers.length)
+                            if (inUsers.length > 1) {
+                                socket.emit("already in")
+                            } else {
+                                nUsers.push(user)
+                                room.users = nUsers
+                                room.save()
+                                socket.join(args.room)
+                                io.to(args.room).emit('msg', args)
+                                io.to(args.room).emit('update user', nUsers)
+                            }
+                        }
+                    });
+                }
+            });
+            //#endregion
+        })
+        .on("disconnecting", () => {
+            console.log(socket.user, "disconnected from room", socket.rooms);
+            socket.rooms.forEach(iRoom => {
+                console.log(iRoom);
+                Room.findOne({ slug: iRoom }, (err, room) => {
+                    if (err) {
+                        console.log(err)
+                    } else if (room) {
+                        User.findById(socket.user.id, (err, user) => {
+                            if (err) {
+                                console.log(err);
+                            } else {
+                                const users = room.get('users') ?? []
+                                const nUsers = users.filter((value, index, array) => {
+                                    return String(user._id) !== String(value._id)
+                                })
+                                room.users = nUsers
+                                console.log("sendeing new Users", room.slug);
+                                io.to(room.slug).emit('update user', nUsers)
+                                room.save()
+                            }
+                        });
+                    }
 
-        io.to(msg.room).emit('msg', msg)
-    });
+                });
+            });
 
-    socket.on('chat', (msg) => {
-        io.emit('chat message', msg)
-    })
+        })
+        .on("disconnect", () => {
+            console.log("diconnect", socket.id)
+        })
 
-    socket.on('conect room', (args) => {
-        socket.join(args.room)
-        io.to(args.room).emit('msg', args)
-    });
-
-    socket.on("disconnect", () => {
-        console.log(socket.id + " Diconnected");
-    });
-    // socket.onAny((eventName, ...args) => {
-    //     console.log(eventName);
-    //     console.log(args);
-    // });
 });
-io.on("disconnect", (socket) => {
-    console.log("Disconnect " + socket.id);
-
-})
 
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/views/index.html")
 })
 
-app
-    .post("/login", (req, res) => {
-        const username = req.body.data.name
-        const password = req.body.data.password
-        User.findOne({ username: username }, (err, doc) => {
-            if (err) {
-                console.log('error in server call to  monngose');
-                res.status(500).json("Error")
-            } else if (doc) {
-                if (doc.get('password') === password) {
-                    res.status(200).json({
-                        name: username,
-                        email: doc.get('email'),
-                        active: true
-                    })
-                } else {
-                    console.log("unknoewn user");
-                    res.status(203).send("Error")
-                }
+
+app.post("/login", (req, res) => {
+    const username = req.body.data.name
+    const password = req.body.data.password
+    User.findOne({ username: username }, (err, doc) => {
+        if (err) {
+            console.log('error in server call to  monngose');
+            res.status(500).json("Error")
+        } else if (doc) {
+            if (doc.get('password') === password) {
+                res.status(200).json({
+                    name: username,
+                    email: doc.get('email'),
+                    id: doc.get('_id'),
+                    active: true
+                })
             } else {
-                res.status(404).send("Error")
+                console.log("unknoewn user");
+                res.status(203).send("Error")
             }
-        })
+        } else {
+            res.status(404).send("Error")
+        }
     })
+})
     .get("/login", (req, res) => {
         res.send().status(404)
     })
-
-
 
 
 server.listen(PORT, () => {
